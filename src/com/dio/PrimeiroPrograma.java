a74004c7aa0f9b7f881a68e10944d6671399e676
Paulo Marcelo <paulomarcelonds@gmail.com>
2022-12-21 13:23:41 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,30 @@
+package com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+	public static void main(String[] args) {
+		
+		
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		
+		
+		/*int a = 956;
+		int b = 1000;
+		
+		System.out.println("Oi Mundo " + (a+b) + " vezes");*/
+	}
+
+}
+
+class Livros{
+	
+	private String nome;
+	private String npag;
+}
+
+
